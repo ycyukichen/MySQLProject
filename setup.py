1c60e0f1c99d3d1e8e4c48663b7a9fe4910eaea0
Yuki Chen <yc0688a@american.edu>
2024-09-05 16:23:01 -0400

    new updated

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+Hyphen_e = '-e .'
+# def get_requirements(filename: str) -> List[str]:
+#     requirements = []
+#     with open(filename) as obj:
+#         requirements = obj.readlines()
+#         requirements = [req.replace("\n", "") for req in requirements]
+        
+#         if Hyphen_e in requirements:
+#             requirements.remove(Hyphen_e)
+    
+#     return requirements
+    
+setup(
+    name="MySQLProject",
+    version="0.1.0",
+    author="Yu-Yu",
+    author_email="yc0688a@american.edu",
+    long_description="MySQL Connect related operations",
+    url=f'https://github.com/ycyukichen/MySQLProject.git',
+    # install_requires=get_requirements("requirements.txt"),
+    package_dir={"": "src"},
+    packages=find_packages(where="src")
+)
