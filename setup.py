218916df1264633a715960df79a842ff3f7a7fdf
Yuki Chen <yc0688a@american.edu>
2024-09-06 15:54:51 -0400

    new updated

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 from typing import List
 
 Hyphen_e = '-e .'
-# def get_requirements(filename: str) -> List[str]:
-#     requirements = []
-#     with open(filename) as obj:
-#         requirements = obj.readlines()
-#         requirements = [req.replace("\n", "") for req in requirements]
+def get_requirements(filename: str) -> List[str]:
+    requirements = []
+    with open(filename) as obj:
+        requirements = obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
         
-#         if Hyphen_e in requirements:
-#             requirements.remove(Hyphen_e)
+        if Hyphen_e in requirements:
+            requirements.remove(Hyphen_e)
     
-#     return requirements
+    return requirements
     
 setup(
     name="MySQLProject",
@@ -20,7 +20,7 @@ setup(
     author_email="yc0688a@american.edu",
     long_description="MySQL Connect related operations",
     url=f'https://github.com/ycyukichen/MySQLProject.git',
-    # install_requires=get_requirements("requirements.txt"),
+    install_requires=get_requirements("requirements.txt"),
     package_dir={"": "src"},
     packages=find_packages(where="src")
 )
